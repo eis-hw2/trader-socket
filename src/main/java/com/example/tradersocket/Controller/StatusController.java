57aa05bf918cfd2a8f930aca6261f35a704fd34a
zzbslayer <zzbslayer@sjtu.edu.cn>
2019-06-10 08:21:13 +0800

    controller to get status

---
+++ src/main/java/com/example/tradersocket/Controller/StatusController.java
@@ -0,0 +1,26 @@
+package com.example.tradersocket.Controller;
+
+import com.example.tradersocket.Domain.Entity.MarketQuotation;
+import com.example.tradersocket.Domain.Factory.ResponseWrapperFactory;
+import com.example.tradersocket.Domain.Wrapper.ResponseWrapper;
+import com.example.tradersocket.Service.BrokerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/Status")
+public class StatusController {
+    @Autowired
+    BrokerService brokerService;
+
+    @GetMapping("/MarketQuotation")
+    public ResponseWrapper getMarketQuotationByBrokerIdAndMarketDepthId(
+            Integer brokerId, String marketDepthId){
+        MarketQuotation mq = brokerService
+                .getDataPairByBrokerIdAndMarketDepthId(brokerId, marketDepthId)
+                .getMarketQuotation();
+        return ResponseWrapperFactory.create(ResponseWrapper.SUCCESS, mq);
+    }
+}
